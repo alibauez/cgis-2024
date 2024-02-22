d384f3bc4dd5d7746ca4bf731ca99c87ba24a9e1
alibauez <alibauez@gmail.com>
2024-02-22 14:03:00 +0100

    atributos cita

--- app/Models/Cita.php
+++ app/Models/Cita.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cita extends Model
 {
-    use HasFactory;
+    //defino los atributos en masa
+    protected $fillable = ['fecha_hora', 'paciente_id' , 'medico_id'];
+
+    //la fecha_hora la quiero almacenar en tipo datetime, por lo que la transformo haciendo uso del casting
+    protected $casts = [
+        'fecha_hora' => 'datetime:Y-m-d H:i',
+    ]
+
+    //ahora establezco las relaciones entre modelos
+
+    public function medico(){ //la clase cita pertenece a clase medico, porq la flecha va hacia este sentido
+        return $this->belongsTo(Medico::class);
+    }
+
+
+    public function paciente(){ //la clase cita pertenece a clase paciente, porq la flecha va hacia este sentido (un paciente tiene cita)
+        return $this->belongsTo(Paciente::class);
+    }
+
+
+
+
+
+
 }
